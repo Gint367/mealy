7510f5c719c4f89f4eb461d6f9103a7806605504
gint367 <rulo.ginting@gmail.com>
2024-12-27 19:47:53 +0100

    cleanup

--- app/shopping-list/page.tsx
+++ app/shopping-list/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { ChevronLeft, ChevronRight, Loader2 } from 'lucide-react'
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Button } from '@/components/ui/button'
 import {
     Collapsible,
